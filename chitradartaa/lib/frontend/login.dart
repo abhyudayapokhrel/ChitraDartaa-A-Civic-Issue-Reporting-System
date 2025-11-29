1a23072c1263fdccff58b96689e81d8bff60b83f
Moss <moss@gmail.com>
2025-11-29 21:08:53 +0545

    boring

---
+++ chitradartaa/lib/frontend/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({super.key});
+
+  @override
+  State<MyLogin> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: Scaffold(
+        
+      )
+    );
+  }
+}
---
+++ chitradartaa/lib/frontend/signup.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({super.key});
+
+  @override
+  State<MyRegister> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyRegister> {
+  @override
+   Widget build(BuildContext context) {
+    return  Container(
+      child: Scaffold(
+        
+      )
+    );
+  }
+}
