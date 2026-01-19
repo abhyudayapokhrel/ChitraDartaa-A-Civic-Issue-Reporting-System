c6ca5489fcd1830bd3477dae6c25b3093b3d24d4
KarnaPratik <pratikgsn@gmail.com>
2026-01-19 14:38:13 +0545

    Fuck you mahesh

--- lib/backend/backend_main.py
+++ lib/backend/backend_main.py
@@ -25,7 +25,7 @@ def create_app():
      methods=["GET", "POST", "OPTIONS"])
 
 
-    #here adding this to add other backend files for inference adding this so i can add later into the future
+    #here adding this to add other backend files for inference adding this so i can add later into the future, fuck you mahesh
     from auth import auth_bp
     app.register_blueprint(auth_bp)
 
